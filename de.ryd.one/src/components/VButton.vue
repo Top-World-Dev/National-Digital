dc37523b41accb5e5c12e3fcd66cdf54f7bb9497
thecameronyoung <cameron@cameronyoung.com.au>
2020-09-24 15:10:46 +1000

    base

--- de.ryd.one/src/components/VButton.vue
+++ de.ryd.one/src/components/VButton.vue
@@ -1,11 +1,13 @@
 <template>
-  <!-- NESTABLE BLOCK // BUTTON -->
-  <div class="xy-button" v-editable="blok">
+  <div class="v-button" v-editable="blok">
     <a :href="blok.link.url" :class="blok.class" :target="blok.linktype != 'url' ? '_blank' : '_self'" :rel="blok.linktype != 'url' ? 'noopener nofollow' : ''">{{ blok.text }}</a>
   </div>
 </template>
-  <script>
-    export default {
-      props: ['blok']
-    }
-  </script>
+<script>
+  export default {
+    props: ['blok']
+  }
+</script>
+<style lang="scss" scoped>
+
+</style>
