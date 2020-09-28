d0eec083d3eca61434d0d1a81a549286401c54a9
thecameronyoung <cameron@cameronyoung.com.au>
2020-09-28 13:32:51 +1000

    section backgrounds

--- de.ryd.one/src/components/BaseColumnX1.vue
+++ de.ryd.one/src/components/BaseColumnX1.vue
@@ -22,14 +22,17 @@
     methods: {
       assignColors(color) {
         switch (color) {
-          case 'is-dark':
-            return `linear-gradient(180deg,rgba(0,0,0,.6),rgba(13,13,13,.6))`
+          case 'is-primary-horizontal':
+            return `linear-gradient(to right, #2e3283 38%, rgba(34, 107, 172, 0.67) 89%)`
             break;
-          case 'is-light':
-            return `linear-gradient(180deg,rgba(255,255,255,.6),rgba(13,13,13,.6))`
+          case 'is-secondary-horizontal':
+            return `linear-gradient(to left, #226bac, #2e3283 0%)`
+            break;
+          case 'is-primary-vertical':
+            return `linear-gradient(to bottom, rgba(46, 50, 131, 0) 47%, #0d0e36 91%)`
             break;
           default:
-            return `linear-gradient(180deg,rgba(13,13,13,.6),rgba(13,13,13,.6))`
+            return `linear-gradient(180deg,rgba(255,255,255,1),rgba(255,255,255,1))`
         } 
       }
     },
