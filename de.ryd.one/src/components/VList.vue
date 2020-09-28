e9533d07508bd19b7acd410143d1fd6922970416
thecameronyoung <cameron@cameronyoung.com.au>
2020-09-28 12:25:27 +1000

    try add list item

---
+++ de.ryd.one/src/components/VList.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul class="v-list" v-editable="blok">
+    <li>
+      <h6 v-if="blok.item.title">{{ blok.item.title }}</h6>
+      <span v-if="blok.item.content">{{ blok.item.content }}</span>
+    </li>
+  </ul>
+</template>
+<script>
+  export default {
+    props: ['blok']
+  }
+</script>
+<style lang="scss">
+@import '~/assets/styles.scss';
+.v-list {
+  @include no-list;
+}
+</style>
