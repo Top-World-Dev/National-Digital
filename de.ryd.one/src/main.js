b307fd19c860d625b4940c9d539beea43be4163e
thecameronyoung <cameron@cameronyoung.com.au>
2020-09-29 08:47:40 +1000

    clean tree

--- de.ryd.one/src/main.js
+++ de.ryd.one/src/main.js
@@ -12,7 +12,7 @@ import BaseColumnX1 from '~/components/BaseColumnX1.vue'
 import BaseColumnX2 from '~/components/BaseColumnX2.vue'
 import BaseColumnX3 from '~/components/BaseColumnX3.vue'
 import VButton from '~/components/VButton.vue'
-import VBlurbIcon from '~/components/VBlurbIcon.vue'
+import VBlurbicon from '~/components/VBlurbicon.vue'
 import VHeading from '~/components/VHeading.vue'
 import VList from '~/components/VList.vue'
 import VRule from '~/components/VRule.vue'
@@ -30,7 +30,7 @@ export default function (Vue, { router, head, isClient }) {
   Vue.component('BaseColumnX2', BaseColumnX2)
   Vue.component('BaseColumnX3', BaseColumnX3)
   Vue.component('VButton', VButton)
-  Vue.component('VBlurbIcon', VBlurbIcon)
+  Vue.component('VBlurbicon', VBlurbicon)
   Vue.component('VHeading', VHeading)
   Vue.component('VList', VList)
   Vue.component('VRule', VRule)
