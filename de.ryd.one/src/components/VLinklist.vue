e434663637e3090c852f6ac7522d65e78250333e
thecameronyoung <cameron@cameronyoung.com.au>
2020-09-29 21:50:57 +1000

    align option for linklist

--- de.ryd.one/src/components/VLinklist.vue
+++ de.ryd.one/src/components/VLinklist.vue
@@ -1,5 +1,5 @@
 <template>
-  <ul class="v-linklist" v-editable="blok" :class="blok.style">
+  <ul class="v-linklist" v-editable="blok" :class="[blok.style,blok.align]">
     <li v-for="item in blok.item" :key="item._uid">
       <g-link v-if="item.link.linktype == 'story'" :to="item.link.url"><v-image v-if="item.image.id" :source="item" class="linklist-icon"></v-image> <span>{{ item.title }}</span></g-link>
       <a v-else :href="item.link.url" rel="noopener noreferrer"><v-image v-if="item.image.id" :source="item" class="linklist-icon"></v-image><span>{{ item.title }}</span></a>
@@ -17,6 +17,7 @@ export default {
   font-size: 0.875rem;
   @include no-list;
   text-align: left;
+  // format
   &.linklist-inline {
     @include nav-row(0.25rem);
     li::before {
@@ -26,6 +27,17 @@ export default {
   &.linklist-block {
     @include nav-col(0.25rem);
   }
+  // align
+  &.linklist-align-left {
+    text-align: left;
+  }
+  &.linklist-align-center {
+    text-align: center;
+  }
+  &.linklist-align-right {
+    text-align: right;
+  }
+  // style
   .linklist-icon {
     display: inline;
     margin-right: 0.25rem;
