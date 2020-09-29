f615275e6a900f1e575b06714eeb23cf78db7ade
tarrah_102 <taradbuckley@gmail.com>
2020-09-29 11:48:06 +1000

    working footer

--- de.ryd.one/src/components/VBlurbicon.vue
+++ de.ryd.one/src/components/VBlurbicon.vue
@@ -13,11 +13,13 @@
     computed: {
       getImage () {
         return this.blok.item.map(item => {
-          if(typeof item.icon == 'object') {
-            item.imageUrl = require(`!!assets-loader?width=100&height=100!@media/${item.icon.filename.filename}`);
-          } else {
-            let image = item.icon.split("/").pop(); 
-            item.imageUrl = require(`!!assets-loader?width=100&height=100!@media/${file}`)
+          if(item.icon) {
+            if(typeof item.icon == 'object') {
+              item.imageUrl = require(`!!assets-loader?width=100&height=100!@media/${item.icon.filename.filename}`);
+            } else {
+              let image = item.icon.split("/").pop(); 
+              item.imageUrl = require(`!!assets-loader?width=100&height=100!@media/${file}`)
+            }
           }
           return item
         });
