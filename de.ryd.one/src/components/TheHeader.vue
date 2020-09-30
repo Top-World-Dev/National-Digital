1218e687d903cd67aff03c7005109b5c3313a795
thecameronyoung <cameron@cameronyoung.com.au>
2020-09-30 12:57:10 +1000

    image not rendering

--- de.ryd.one/src/components/TheHeader.vue
+++ de.ryd.one/src/components/TheHeader.vue
@@ -1,16 +1,15 @@
 <template>
   <header>
-    <component :key="blok._uid" v-for="blok in blok.section.content.section" :blok="blok" :is="blok.component"></component> 
+    {{ blok }}
   </header>
 </template>
 
 <script>
-  export default {
-    props: ['blok'],
-  }
+export default {
+  props: ["blok"],
+};
 </script>
 
 <style lang="scss">
-@import '~/assets/styles.scss';
-
+@import "~/assets/styles.scss";
 </style>
