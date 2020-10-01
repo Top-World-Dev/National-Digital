6e00dfe3b1812aeab578369a9c0ec2b5ffbac6c7
thecameronyoung <cameron@cameronyoung.com.au>
2020-10-01 12:36:22 +1000

    mobile styles

--- de.ryd.one/src/components/BaseSectionColumn.vue
+++ de.ryd.one/src/components/BaseSectionColumn.vue
@@ -1,42 +1,62 @@
 <template>
   <section :id="blok.name" class="xy-column" :class="blok.valign">
-      <div class="column-inner" :class="[blok.container,blok.align]">
-        <component :key="blok._uid" v-for="blok in blok.block" :blok="blok" :is="blok.component"></component>
-      </div>
+    <div
+      class="column-inner column-container"
+      :class="[blok.container, blok.align]"
+    >
+      <component
+        :key="blok._uid"
+        v-for="blok in blok.block"
+        :blok="blok"
+        :is="blok.component"
+      ></component>
+    </div>
   </section>
 </template>
 
 <script>
-  import backgroundImage from '../mixins/backgroundImage'
-  export default {
-    mixins: [backgroundImage],
-    props: ['blok'],
-  }
+import backgroundImage from "../mixins/backgroundImage";
+export default {
+  mixins: [backgroundImage],
+  props: ["blok"],
+};
 </script>
 
 <style lang="scss">
-@import '~/assets/styles.scss';
+@import "~/assets/styles.scss";
 .xy-column {
-  /* Vertical alignment */
-  &.column-valign-top {
-    align-self: flex-start;
+  /* Layouts */
+  // mobile defaults
+  @media (max-width: $breakColumns - 1px) {
+    .column-container {
+      width: 100%;
+      height: auto;
+    }
   }
-  &.column-valign-middle {
-    align-self: center;
-  }
-  &.column-valign-bottom {
-    align-self: flex-end;
-  }
-  /* Container Options */
-  .column-container-none {
-    width: 100%;
-    height: auto;
-  }
-  .column-container-mleft {
-    margin-left: 40%;
-  }
-  .column-container-mright {
-    margin-right: 40%;
+  @media (min-width: $breakColumns) {
+    /* Vertical alignment (desktop) */
+    &.column-valign-top {
+      align-self: flex-start;
+    }
+    &.column-valign-middle {
+      align-self: center;
+    }
+    &.column-valign-bottom {
+      align-self: flex-end;
+    }
+    /* Container Options */
+    .column-container-none {
+      width: 100%;
+      height: auto;
+    }
+    .column-container-mleft {
+      margin-left: 40%;
+      margin-right: 1.5rem;
+    }
+    .column-container-mright {
+      margin-left: 1.5rem;
+      margin-right: 40%;
+    }
   }
   /* Text alignment */
   .column-align-left {
