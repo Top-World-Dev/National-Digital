2c292d3c3e41d9b165c0cb53b9c33f4e165a4076
thecameronyoung <cameron@cameronyoung.com.au>
2020-10-01 20:17:07 +1000

    slider container

---
+++ de.ryd.one/src/components/VSlider.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="v-slider" :class="blok.style" v-editable="blok">
+    {{ blok }}
+  </div>
+</template>
+<script>
+  export default {
+    props: ['blok']
+  }
+</script>
+<style lang="scss">
+@import '~/assets/styles.scss';
+.v-slider {
+  
+}
+</style>
