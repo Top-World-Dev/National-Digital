ce1dfb8043653e9224c78453be3be5be4cda2415
tarrah_102 <taradbuckley@gmail.com>
2020-10-01 10:57:33 +1000

    added background image

--- de.ryd.one/src/components/VImage.vue
+++ de.ryd.one/src/components/VImage.vue
@@ -9,12 +9,20 @@ export default {
   props: ["source"],
   computed: {
     image() {
-      let imageName = (typeof this.source.image == 'object') ? this.source.image.filename.filename : this.source.image.split("/").pop();
-      let image = require(`!!assets-loader!@media/${imageName}`);
-      image.size.height = parseInt(this.source.height);
-      image.size.width = parseInt(this.source.width);
-      image.alt = this.source.title;
-      return image;
+      // check if it's an svg 
+      if(typeof this.source.image.filename == 'string') {
+        let filetype = this.source.image.filename.split('.').pop();
+        if(filetype == 'svg') {
+          return this.source.image.filename;
+        }
+      } else {
+        let imageName = this.source.image.filename.filename;
+        let image = require(`!!assets-loader!@media/${imageName}`);
+        image.alt = this.source.title;
+        image.size.height = parseInt(this.source.height);
+        image.size.width = parseInt(this.source.width);
+        return image;
+      }
     },
   },
 };
--- de.ryd.one/src/mixins/backgroundImage.js
+++ de.ryd.one/src/mixins/backgroundImage.js
@@ -17,13 +17,21 @@ export default {
         default:
           return `linear-gradient(0deg, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0) 100%)`
       }
+    },
+    getImage(asset) {
+      let image = require(`!!assets-loader!@media/${this.blok.backgroundImage.filename.filename}`);
+      return image.src;
     }
   },
   computed: {
     backgroundStyle() {
+      if(typeof this.blok.backgroundImage.filename == 'object') {
+        let filename = this.blok.backgroundImage.filename.filename
+      }
+
       let color = this.assignColors(this.blok.backgroundColor);
-      //       return (!this.blok.backgroundImage.filename) ? `${color}` : `${color}`;
-      return color;
-    }
+      return (typeof this.blok.backgroundImage.filename == 'object') ? `${color}, url('${this.getImage(this.blok.backgroundImage.filename.filename)}')` : `${color}`
+    
+    },
   }
 };
