5e01316be3a31e4fd06dd1f957da1ae69cd1bba9
thecameronyoung <cameron@cameronyoung.com.au>
2020-10-05 11:22:49 +1000

    slider

--- de.ryd.one/src/components/VSlider.vue
+++ de.ryd.one/src/components/VSlider.vue
@@ -1,59 +1,98 @@
 <template>
   <div class="v-slider" :class="blok.style" v-editable="blok">
-      <VueSlickCarousel v-bind="carouselSettings">
-        <div v-for="slide in blok.slide" :key="blok._uid">
-          <component :key="blok._uid" v-for="blok in slide.block" :blok="blok" :is="blok.component"></component>
-        </div>
-      </VueSlickCarousel>
+    <VueSlickCarousel v-bind="carouselSettings">
+      <div v-for="slide in blok.slide" :key="blok._uid">
+        <component :key="blok._uid" v-for="blok in slide.block" :blok="blok" :is="blok.component"></component>
+      </div>
+    </VueSlickCarousel>
   </div>
 </template>
 <script>
-  import 'vue-slick-carousel/dist/vue-slick-carousel.css'
-  // optional style for arrows & dots
-  import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
-  export default {
-    props: ['blok'],
-    components: {
+import "vue-slick-carousel/dist/vue-slick-carousel.css";
+// optional style for arrows & dots
+import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
+export default {
+  props: ["blok"],
+  components: {
     VueSlickCarousel: () =>
       import("vue-slick-carousel")
-      .then((component) => component)
-      .catch(),
-    },
-    data() {
-      return {
-        carouselSettings: {
-          centerMode: true,
-          focusOnSelect: true,
-          infinite: true,
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          speed: 500,
-          dots: false,
-          dotsClass: "slick-dots",
-          arrows: true,
-          responsive: [
-            {
-              breakpoint: 768,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-              },
+        .then((component) => component)
+        .catch(),
+  },
+  data() {
+    return {
+      carouselSettings: {
+        accessibility: true,
+        adaptiveHeight: false,
+        arrows: true,
+        asNavFor: null,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        centerMode: true,
+        centerPadding: "0px",
+        cssEase: "ease",
+        customPaging: undefined,
+        dots: false,
+        dotsClass: "slick-dots",
+        draggable: true,
+        edgeFriction: 0.35,
+        fade: false,
+        focusOnSelect: true,
+        infinite: true,
+        initialSlide: 0,
+        lazyLoad: null,
+        pauseOnDotsHover: false,
+        pauseOnFocus: false,
+        pauseOnHover: true,
+        rows: 1,
+        rtl: false,
+        slidesPerRow: 1,
+        slidesToScroll: 1,
+        slidesToShow: 3,
+        speed: 500,
+        swipe: true,
+        swipeToSlide: false,
+        touchMove: true,
+        touchThreshold: 5,
+        useCSS: true,
+        useTransform: true,
+        variableWidth: false,
+        vertical: false,
+        waitForAnimate: true,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
             },
-            {
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-              },
-            }
-          ],
-        }
-      }
-    }
-  }
+          },
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            },
+          },
+        ],
+      },
+    };
+  },
+};
 </script>
 <style lang="scss">
 @import '~/assets/styles.scss';
 .v-slider {
+  .slick-arrow {
+    &::before {
+      color: $accent;
+    }
+    &.slick-prev {
+      left: -0.5rem;
+    }
+    &.slick-next {
+      right: -0.5rem;
+    }
+  }
 }
 </style>
