3335d3c0914bf5147beff69a7bf2f90c70a6d1fc
tarrah_102 <taradbuckley@gmail.com>
2020-10-08 13:34:54 +1000

    changed editor.vue

--- src/pages/Editor.vue
+++ src/pages/Editor.vue
@@ -10,38 +10,24 @@
 </template>
 
 <script>
-const getParam = function(val) {
-  var result = '',
-      tmp = []
-  location.search
-    .substr(1)
-    .split('&')
-    .forEach(function (item) {
-      tmp = item.split('=')
-      if (tmp[0] === val) result = decodeURIComponent(tmp[1])
-  })
-  return result
-}
+import config from '../../gridsome.config'
 
 const loadStoryblokBridge = function(cb) {
+  let sbConfigs = config.plugins.filter((item) => {
+    return item.use === 'gridsome-source-storyblok'
+  })
+  let sbConfig = sbConfigs.length > 0 ? sbConfigs[0] : {}
   let script = document.createElement('script')
   script.type = 'text/javascript'
-  script.src = `//app.storyblok.com/f/storyblok-latest.js?t=${getParam('token')}`
+  script.src = `//app.storyblok.com/f/storyblok-latest.js?t=${sbConfig.options.client.accessToken}`
   script.onload = cb
   document.getElementsByTagName('head')[0].appendChild(script)
 }
 
 export default {
-  name: 'EditorPage',
   data() {
     return {
-      story: {content: {}},
-      oldPath: ''
-    }
-  },
-  computed: {
-    globalData () {
-      return this.$page.global.edges[0].node
+      story: {content: {}}
     }
   },
   mounted() {
@@ -49,31 +35,29 @@ export default {
   },
   methods: {
     loadStory() {
-      const path = getParam('path')
-
-      if (path !== '') {
-        this.oldPath = path
-      }
-
       window.storyblok.get({
-        slug: path || this.oldPath,
-        version: 'draft'
+        slug: window.storyblok.getParam('path'),
+        version: 'published'
       }, (data) => {
         this.story = data.story
       })
     },
     initStoryblokEvents() {
       this.loadStory()
+
       let sb = window.storyblok
+
       sb.on(['change', 'published'], (payload) => {
         this.loadStory()
       })
+
       sb.on('input', (payload) => {
         if (this.story && payload.story.id === this.story.id) {
           payload.story.content = sb.addComments(payload.story.content, payload.story.id)
           this.story = payload.story
         }
       })
+
       sb.pingEditor(() => {
         if (sb.inEditor) {
           sb.enterEditmode()
