fcb9edc8f34e0fa2252f221e4885db94f4dcbdfa
tarrah_102 <taradbuckley@gmail.com>
2020-10-12 15:07:25 +1000

    mapping base functionality complete

--- src/components/VMap.vue
+++ src/components/VMap.vue
@@ -1,15 +1,21 @@
 <template>
 
-<div style="height: 500px; width: 100%">
-
+<div class="v-search">
     <div class="search-form">
       <div class="search-row">
-        <input type="search" name="search" placeholder="Search" class="search-input" @keyup="search($event.target.value)">
-        <a role="button" class="search-clear search-is-hidden" tabindex="0"></a>
+        <input type="text" name="search" placeholder="Search" class="search-input" v-model="searchValue" @keyup="search($event.target.value)" @keyup.enter="loadResults">
+        <a role="button" class="search-clear" tabindex="0" @click="searchValue = ''" ></a>
+      </div>
+      <div class="search-results" v-if="searchResults.length > 0">
+        <div class="search-result" v-for="result in searchResults" @click="findResult(result)">
+          <p><strong>{{ result.title }}</strong></p>
+          <p >{{ result.address }}</p>
+        </div>
       </div>
     </div>
     <l-map
       v-if="showMap"
+      ref="map"
       :zoom="zoom"
       :center="center"
       :options="mapOptions"
@@ -20,12 +26,12 @@
         :attribution="attribution"
       />
       <l-marker
-        v-for="location in locations"
-        :key="location.id"
-        :lat-lng.sync="location.position"
-        @click="alert(location)"
+        v-for="marker in markers"
+        :key="marker.id"
+        :lat-lng.sync="marker.position"
+        @click="goToMarker(marker.position)"
       >
-        <l-tooltip :content="location.tooltip" />
+        <l-tooltip :content="marker.tooltip" />
       </l-marker>
     </l-map>
   </div>
@@ -66,6 +72,8 @@ export default {
         address: location.address
       }
     });
+    // only load markers that are in view
+    this.markers = this.locations.filter(point => this.$refs.map.mapObject.getBounds().contains(point.position))
     this.loading = false;
   },
   data() {
@@ -76,27 +84,98 @@ export default {
       url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       attribution:
         '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      currentZoom: 11.5,
-      currentCenter: latLng(52.520008, 13.404954),
-      showParagraph: false,
       mapOptions: {
         zoomSnap: 0.5
       },
       showMap: true,
       locations: [],
+      markers: [],
+      searchValue: '',
+      searchResults: []
     };
   },
   methods: {
     search(value) {
       if(value.length > 2) {
-        console.log(this.locations.filter(item => item.title.toLowerCase().indexOf(value.toLowerCase()) != -1 || item.tooltip.toLowerCase().indexOf(value.toLowerCase()) != -1 || item.address.toLowerCase().indexOf(value.toLowerCase()) != -1));
+        this.searchResults = this.locations.filter(item => item.tooltip.toLowerCase().indexOf(value.toLowerCase()) != -1 || item.address.toLowerCase().indexOf(value.toLowerCase()) != -1)
       }
-     
+    },
+    findResult(result) {
+      this.searchResults = [];
+      this.markers = [];
+      this.markers.push(result);
+      this.$refs.map.mapObject.flyTo(latLng(result.position.lat, result.position.lng),14, { animate: true, duration: 0.5});
+    },
+    loadResults() {
+      this.markers = [];
+      this.searchResults.forEach(result => this.markers.push(result));
+      this.searchResults = [];
+      this.$refs.map.mapObject.fitBounds(this.markers.map(m => [m.position.lat, m.position.lng]))
+    },
+    goToMarker(marker) {
+      this.$refs.map.mapObject.flyTo(latLng(marker.lat, marker.lng), 14, { animate: true, duration: 0.5});
     }
   }
 };
 </script>
 
-<style>
+<style lang="scss">
+.search-results {
+  background-color: white;
+}
+
+.search-result {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-bottom: 1px solid black;
+    p {
+      margin-left:12px;
+      
+    }
+
+
+}
+.search-form {
+  position: absolute;
+  top: 3rem;
+  z-index: 999;
+  margin-left: auto;
+  margin-right: auto;
+  left: 0;
+  right: 0;
+  width: 30vw;
+}
+.search-row {
+  position: relative;
+}
+.search-input {
+  width: 100%;
+}
+.search-clear {
+  position: absolute;
+  right: 1rem;
+  top: 0.2rem;
+  width: 15px;
+  height: 15px;
+  opacity: 0.8;
+}
+.search-clear:hover {
+  opacity: 1;
+}
+.search-clear:before, .search-clear:after {
+  position: absolute;
+  left: 15px;
+  content: ' ';
+  height: 16px;
+  width: 2px;
+  background-color: #333;
+}
+.search-clear:before {
+  transform: rotate(45deg);
+}
+.search-clear:after {
+  transform: rotate(-45deg);
+}
 
 </style>
