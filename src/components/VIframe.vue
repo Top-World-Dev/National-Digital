2944c29e86c768c1eaf1ca53176435150cd7a236
tarrah_102 <taradbuckley@gmail.com>
2020-10-15 14:35:45 +1000

    getting xml data of the job positions

--- src/components/VIframe.vue
+++ src/components/VIframe.vue
@@ -1,7 +1,8 @@
 <template>
-  <iframe class="v-iframe" :id="blok.id" :src="blok.src" :style="{height: height}"></iframe>
+  <!-- <iframe class="v-iframe" :id="blok.id" :src="blok.src" :style="{height: height}"></iframe> -->
 </template>
 <script>
+  import xmlToJson from '../xml-json';
   export default {
     props: ['blok'],
     data() {
@@ -9,15 +10,16 @@
         height: '0px'
       }
     },
-    mounted() {
-      window.addEventListener('message', (event) => {
-        this.height = `${event.data[1]}px`;
-      });
-    },
     destroyed() {
       window.removeEventListener('resize', (event) => {
       });
     },
+    async mounted() {
+      const response = await fetch('https://thinxnet-jobs.personio.de/xml');
+      const data = await response.text();
+      var XmlNode = new DOMParser().parseFromString(data, 'text/xml');
+      console.log(XmlNode)
+    }   
   }
 </script>
 <style lang="scss">
