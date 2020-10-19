e13faccbc06f408f9c305f5477418a430baa628d
thecameronyoung <cameron@cameronyoung.com.au>
2020-10-19 14:17:34 +1000

    fix multi-column layout

--- src/components/BaseSection.vue
+++ src/components/BaseSection.vue
@@ -1,7 +1,7 @@
 <template>
   <section :id="blok.name" class="xy-section" :style="backgroundStyle ? {backgroundImage: backgroundStyle} : ''" :class="blok.backgroundColor" v-editable="blok">
-      <div class="section-inner section-container" :class="[blok.container,blok.align,(blok.reverse ? 'column-reverse' : '' ),(blok.mobile_fullwidth ? 'column-fullmobile' : '')]">
-        <component :key="blok._uid" v-for="blok in blok.columns" :blok="blok" :is="blok.component" :class="`column-${countCols}`"></component>
+      <div class="section-inner section-container" :class="[blok.container,blok.align,`section-${countCols}`,(blok.reverse ? 'column-reverse' : '' ),(blok.mobile_fullwidth ? 'column-fullmobile' : '')]">
+        <component :key="blok._uid" v-for="blok in blok.columns" :blok="blok" :is="blok.component"></component>
       </div>
   </section>
 </template>
@@ -86,31 +86,31 @@
   // desktop options
   @media (min-width: $breakColumns) {
     .section-container-none {
-      // width: 100%;
-      // height: auto;
+      width: 100%;
+      height: auto;
     }
     .section-container-large {
       @include container($containerOuter);
-      > * {
+      &.section-multi > * {
         margin: 1rem;
       }
     }
     .section-container-normal {
       @include container($container);
-      > * {
+      &.section-multi > * {
         margin: 1rem;
       }
     }
     .section-container-small {
       @include container($containerInner);
-      > * {
+      &.section-multi > * {
         margin: 1rem;
       }
     }
   }
   /* Flex layouts - mobile */
   @media (max-width: $breakColumns - 1px) {
-    .section-inner {
+    .section-multi.section-inner {
       display: flex;
       flex-direction: column;
       &.column-reverse {
@@ -120,7 +120,7 @@
   }
   /* Flex layouts - desktop */
   @media (min-width: $breakColumns) {
-    .section-inner {
+    .section-multi.section-inner {
       display: flex;
       flex-direction: row;
       > * {
